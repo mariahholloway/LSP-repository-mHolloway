2744154afd51129170ef65cc3897d797e5fe602a
sweet <sweet@172.17.18.233>
2022-01-26 20:40:42 -0500

    k

---
+++ LSP_asignment1/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,9 @@
+package org.howard.edu.lsp.assignment1;
+//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
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
